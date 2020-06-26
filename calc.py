9d7dd531841aba517db00d4667f5a014a4387106
uzjaee <ol8429@kookmin.ac.kr>
2020-06-26 22:58:13 +0900

    issue #3 add calc.py :calulate add and multiply add calc.conf: activating apache2 server by calc.py add template.py :template html python file

---
+++ calc.py
@@ -0,0 +1,25 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ,start_response):
+        d=parse_qs(environ['QUERY_STRING'])
+        a=d.get('a' ,[''])[0]
+        b=d.get('b' ,[''])[0]
+        add=0
+        mul=0
+      if ''not in [a,b]:
+                a,b= int(a) ,int(b)
+
+                add=a+b
+                mul=a*b
+
+        response_body =html %{
+                'add' : add,
+                'mul' : mul
+        }
+        start_response('200 OK', [
+                ('Content-Type', 'text/html'),
+                ('Content-Length', str(len(response_body)))
+        ])
+        return [response_body]
+
---
+++ template.py
@@ -0,0 +1,18 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            if you want to add and multiply any two number
+            press any two number <br><br>
+            a = <input type="number" name="a">
+            b = <input type="number" name="b">
+
+           <input type="submit">
+        </form>
+        <p>
+          add= %(add)d</br>
+          mul= %(mul)d</br>
+        </p>
+    </body>
+</html>
+
